5e38c822c79ef45fdfb4c80ebfc060c389d85d6c
Fusbydev <kirkfabonnnn@gmail.com>
2023-11-02 11:16:56 +0800

    added editInventory.java and button

---
+++ app/src/main/java/com/example/practicecrude/editInventory.java
@@ -0,0 +1,13 @@
+package com.example.practicecrude;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editInventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_inventory);
+    }
+}
